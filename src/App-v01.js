98d20641b32f63ca5715ca367c0a526dda28c8fe
Elena Cristescu <elenacristescu11@gmail.com>
2024-02-24 16:23:45 +0100

    make the buttons into a reauseable component which passes a children prop for the content

--- src/App-v01.js
+++ src/App-v01.js
@@ -36,31 +36,22 @@ export default function App() {
           </p>
 
           <div className="buttons">
-            <Button
-              textColor={"#fff"}
-              bgColor={"#7950f2"}
+            <button
+              style={{ backgroundColor: "#7950f2", color: "#fff" }}
               onClick={handlePrevious}
             >
-              <span>◀️</span>Previous
-            </Button>
+              Previous
+            </button>
 
-            <Button textColor={"#fff"} bgColor={"#7950f2"} onClick={handleNext}>
-              Next<span>▶️</span>
-            </Button>
+            <button
+              style={{ backgroundColor: "#7950f2", color: "#fff" }}
+              onClick={handleNext}
+            >
+              Next
+            </button>
           </div>
         </div>
       )}
     </>
-  );
-}
-
-function Button({ textColor, bgColor, onClick, children }) {
-  return (
-    <button
-      style={{ backgroundColor: bgColor, color: textColor }}
-      onClick={onClick}
-    >
-      {children}
-    </button>
   );
 }
